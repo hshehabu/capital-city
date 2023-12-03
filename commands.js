4cacce50811f60b1327bdf638fd48646e462f5e1
Ibnu shihab <86791773+hshehabu@users.noreply.github.com>
2023-12-03 11:33:28 +0300

    Update commands.js

--- commands.js
+++ commands.js
@@ -1,5 +1,5 @@
 const commands = [
-    {command :"/capital" , description : "look for capital cities all over the world"},
+    {command :"/capital_city" , description : "look for capital cities all over the world"},
     {command : "help" , description : "if you seek help"}
 ]
 module.exports = commands
